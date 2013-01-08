24507516545f9f2e83acd8e52ea57d7c4688769f
Brian Runnells <brian.runnells@gmail.com>
2013-01-08 17:24:35 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hardclimbs::Application.config.secret_token = '534b57d6613364f0ce2a9851067f84bc3c3bda8812b752f7f1b7219d9db7dcc9a85d6fb2fa01f7a3b124be06bfb6f6397442edd32995db26c6f1ea9e15a6f528'
