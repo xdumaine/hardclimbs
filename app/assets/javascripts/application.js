8d973a6d1a66a1421b0b3f7ac52771d3289bed18
Brian Runnells <brian.runnells@gmail.com>
2013-01-16 20:14:08 -0600

    Fix stylesheet issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
-//#= require twitter/bootstrap/bootstrap-transition
-//#= require twitter/bootstrap/bootstrap-alert
-//#= require twitter/bootstrap/bootstrap-modal
-//#= require twitter/bootstrap/bootstrap-button
-//#= require twitter/bootstrap/bootstrap-collapse
-//#= require twitter/bootstrap/bootstrap-dropdown
+//= require bootstrap
 //= require_tree .
 
