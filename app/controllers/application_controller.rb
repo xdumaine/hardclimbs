4cade8baf8251b1ea4b0a764763b52f3bb41f7d1
Brian Runnells <brian.runnells@gmail.com>
2013-05-23 17:37:29 -0500

    Climber comments, 404 error page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+  
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActiveRecord::RecordNotFound,
+                ActionController::RoutingError,
+                ActionController::UnknownController,
+                ActionController::UnknownAction,
+                ActionController::MethodNotAllowed do |exception|
+      routing_error
+    end
+  end
+      
+  def routing_error
+      render "pages/404", :status => 404
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -86,4 +86,7 @@ Hardclimbs::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  # Any routes that aren't defined above here go to the 404
+  match "*a", :to => "application#routing_error"
+  
 end
