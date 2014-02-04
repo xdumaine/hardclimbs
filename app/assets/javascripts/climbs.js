24eed836518b95557fc48b97ad59a56c88fc1af1
Brian Runnells <brian.runnells@gmail.com>
2014-02-04 08:08:57 -0700

    Fix #14

--- app/assets/javascripts/climbs.js
+++ app/assets/javascripts/climbs.js
@@ -2,7 +2,7 @@ $(function() {
 	$('.chzn-select').chosen()
 	$("a[rel=popover]").popover()
 	$("a[rel=tooltip]").tooltip()
-    $("#climb-list .table th a, .pagination a").live("click", function() {
+    $("#climb-list .table th a, .pagination a").on("click", function() {
       $.getScript(this.href);
       return false;
     });
