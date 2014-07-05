4010fd16756e36c7656783495e0377bee82fd8b8
Brian Runnells <brian.runnells@gmail.com>
2014-07-05 12:55:19 -0600

    Add bootstrap css/js

--- ember/Brocfile.js
+++ ember/Brocfile.js
@@ -18,3 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 module.exports = app.toTree();
+
+app.import('vendor/bootstrap/dist/js/bootstrap.js');
+app.import('vendor/bootstrap/dist/css/bootstrap.css');
