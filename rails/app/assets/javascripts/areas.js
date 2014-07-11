0e5053393ef08a556dc35505978214d267d67e0d
Brian Runnells <brian.runnells@gmail.com>
2014-07-10 21:55:04 -0600

    Remove unneeded files

--- rails/app/assets/javascripts/areas.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$(".chzn-select").chosen()
-	$("a[rel=popover]").popover()
-	$("a[rel=tooltip]").tooltip()
-    $("#area-list .table th a, .pagination a").on("click", function() {
-      $.getScript(this.href);
-      return false;
-    });
-});
--- rails/app/assets/javascripts/home.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$('#mediaCarousel').carousel({
-  	  interval: 4000,
-	  pause: "hover"
-	});
-});
