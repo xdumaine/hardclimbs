11cfb0ec15eb82c5ba4aebff4796e1cef1280531
Brian Runnells <brian.runnells@gmail.com>
2014-07-11 22:17:50 -0600

    Climber index

--- ember/app/models/climber.js
+++ ember/app/models/climber.js
@@ -5,6 +5,7 @@ var attr = DS.attr;
 export default DS.Model.extend({
   first_name: attr('string'),
   last_name: attr('string'),
+  full_name: attr('string'),
   sex: attr('string'),
   dob: attr('date'),
   height: attr(),
--- rails/app/serializers/climber_serializer.rb
+++ rails/app/serializers/climber_serializer.rb
@@ -2,6 +2,6 @@ class ClimberSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
   attributes :id, :first_name, :last_name, :slug, :created_at, :updated_at, :dob, :sex,
-              :ascents_count, :climbs_count, :height
+              :ascents_count, :climbs_count, :height, :full_name
   has_many :ascents
 end
