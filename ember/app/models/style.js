0ecca23e7f92bda5b90f3e9fd257117b7d80f8ac
Brian Runnells <brian.runnells@gmail.com>
2014-07-13 10:19:22 -0500

    Styles model

--- ember/app/models/style.js
+++ ember/app/models/style.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-
+  name: attr('string'),
+  still_hard: attr('boolean'),
+  notes: attr('string'),
+  style: DS.belongsTo('style'),
+  area: DS.belongsTo('area'),
+  grade: DS.belongsTo('grade')
 });
