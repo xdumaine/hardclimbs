25e758b25541349bf4cafdc9fd076707c9c75488
Brian Runnells <brian.runnells@gmail.com>
2014-07-19 08:20:52 -0600

    Clean up style model

--- ember/app/models/style.js
+++ ember/app/models/style.js
@@ -3,10 +3,5 @@ import DS from 'ember-data';
 var attr = DS.attr;
 
 export default DS.Model.extend({
-  name: attr('string'),
-  still_hard: attr('boolean'),
-  notes: attr('string'),
-  style: DS.belongsTo('style'),
-  area: DS.belongsTo('area'),
-  grade: DS.belongsTo('grade')
+  name: attr('string')
 });
